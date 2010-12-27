21f73a42a86cf7eb0201061af937566d0b194b7d
Tony Cook <tony@develop-help.com>
2010-12-27 15:21:27 +1100

    branches fixed

--- svnxlate.pl
+++ svnxlate.pl
@@ -6,18 +6,39 @@ use feature 'switch';
 my $dump = SVN::Dump->new({ file => "-" });
 
 my $iscvstag = 0;
+my $iscvsbranch = 0;
+my $revnum = 0;
+my $pending;
+my @pending;
+my $pending_rev;
 while (my $rec = $dump->next_record) {
   my $type = $rec->type;
   print STDERR "Record: ", $rec->type, "\n";
   my $action = "KEEP";
   given ($type) {
     when ("revision") {
-      print STDERR "Revision ", $rec->get_header('Revision-number'), "\n";
-      $iscvstag = $rec->get_property("svn:log") =~ /^This commit was manufactured by cvs2svn to create tag/;
+      if ($pending) {
+	end_pending(\@pending, $pending_rev);
+	$pending = 0;
+      }
+      my $revnum = $rec->get_header('Revision-number');
+      print STDERR "Revision $revnum\n";
+      #$revnum == 165 and $DB::single = 1;
+      my $log = $rec->get_property("svn:log");
+      $iscvstag = $log =~ /^This commit was manufactured by cvs2svn to create tag/;
+      $iscvsbranch = $log =~ /^This commit was manufactured by cvs2svn to create branch/;
+
+      if ($iscvstag || $iscvsbranch) {
+	$pending = 1;
+	$pending_rev = $revnum-1;
+	$action = "SKIP";
+      }
     }
     when ("node") {
       my $path = $rec->get_header('Node-path');
       my $outpath = $path;
+      my $kind = $rec->get_header('Node-kind') || '';
+      my $copyfrom = $rec->get_header("Node-copyfrom-path") || "";
       $action = "SKIP";
       if ($path =~ /^(trunk|branches|tags)$/
 	 || $path =~ m(^trunk/Imager(/|$))
@@ -40,12 +61,25 @@ while (my $rec = $dump->next_record) {
 	}
       }
       if ($iscvstag &&
-	  $rec->get_header("Node-kind") eq "file") {
+	  $kind eq "file") {
+	$action = "SKIP";
+      }
+      if ($iscvsbranch &&
+	  $kind eq "file") {
 	$action = "SKIP";
       }
       if ($iscvstag) {
-	if ($rec->get_header("Node-kind") eq "dir"
+	if ($kind eq "dir"
 	  && $rec->get_header("Node-copyfrom-path") eq "branches/devel") {
+	  $rec->set_header("Node-copyfrom-path", "trunk/Imager");
+	}
+	else {
+	  $action = "SKIP";
+	}
+      }
+      if ($iscvsbranch) {
+	if ($kind eq "dir" && ($copyfrom eq "branches/devel" || $copyfrom eq "trunk")) {
+print STDERR "New branch $outpath\n";
 	  $rec->set_header("Node-copyfrom-path", "trunk/Imager");
 	}
 	else {
@@ -56,9 +90,10 @@ while (my $rec = $dump->next_record) {
       if ($outpath =~ s(^branches/Imager/)(branches/)) {
 	$rec->set_header('Node-path', $outpath);
       }
-#       elsif ($outpath =~ s((^branches/\w+/)Imager/)($1)) {
-# 	$rec->set_header('Node-path', $outpath);
-#       }
+      elsif ($outpath =~ s((^branches/\w+/)Imager/)($1)) {
+ 	$rec->set_header('Node-path', $outpath);
+print STDERR "  Branch rename $outpath\n";
+      }
       fix_paths($rec);
       my $inc = $rec->get_included_record;
       if ($inc) {
@@ -76,15 +111,34 @@ while (my $rec = $dump->next_record) {
       print STDERR "$type\n";
     }
   }
+  if ($pending) {
+    push @pending, $rec;
+    my $copyrev = $rec->get_header("Node-copyfrom-rev");
+    if ($copyrev && $copyrev > $pending_rev) {
+      $pending_rev = $copyrev;
+    }
+    $action = "SKIP";
+  }
   if ($action eq "KEEP") {
+    print_recs($rec);
+  }
+  print STDERR "EndRecord\n";
+}
+
+if (@pending) {
+  end_pending(\@pending, $pending_rev);
+}
+
+sub print_recs {
+  my (@recs) = @_;
+
+  for my $rec (@recs) {
     my $text = $rec->as_string;
     $text =~ s/^Content-length: 0\n//m;
     $text =~ s/^Prop-content-length: 0\n//m;
     print $text;
   }
-  print STDERR "EndRecord\n";
 }
-
 
 sub fix_paths {
   my ($rec) = @_;
@@ -112,4 +166,16 @@ sub fix_paths {
       print STDERR "Newcopyfrom: $copypath\n";
     }
   }
+}
+
+sub end_pending {
+  my ($pending, $rev) = @_;
+
+  # discard the individual copies
+  splice(@$pending, 2);
+
+  $pending->[1]->set_header("Node-copyfrom-rev", $rev);
+  print_recs(@$pending);
+
+  @$pending = ();
 }
--- trysvnfilt.sh
+++ trysvnfilt.sh
@@ -2,14 +2,15 @@
 rm -rf svnwork
 svnadmin create svnwork
 perl svnxlate.pl <svndump.txt >svndumpfilt.txt
-svnadmin load svnwork/ <svndumpfilt.txt
+svnadmin load svnwork/ <svndumpfilt.txt || exit 1
+svn ls file://`pwd`/svnwork/branches/antialias || exit 1
 rm -rf gitwork
-git svn clone file://`pwd`/svnwork/ --no-metadata -s -Ttrunk/Imager -A gitauthor.txt --prefix svn/ gitwork
+git svn clone file://`pwd`/svnwork/ --no-metadata -s -Ttrunk/Imager -A gitauthor.txt --prefix svn/ gitwork || exit 1
 cd gitwork
 git svn-abandon-fix-refs
 git svn-abandon-cleanup
 git config --remove-section svn
 git config --remove-section svn-remote.svn
 # empty branch left from having some branches under branches/Imager
-git co antialias || ( echo No anti-alias! ; exit 1 )
+git checkout antialias || ( echo No anti-alias! ; exit 1 )
 git branch -D Imager
